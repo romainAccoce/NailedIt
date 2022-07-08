f212a908fbf1e38c5ca29afa3235a9195123560f
romainAccoce <romainaccoce@gmail.com>
2022-07-08 17:28:11 +0200

    add axios

--- src/components/SearchField/index.tsx
+++ src/components/SearchField/index.tsx
@@ -1,9 +1,10 @@
 import { useState } from "react";
+import axios from "axios";
 
 const SearchField: React.FC = () => {
     const [searchKey, setSearchKey] = useState('');
     const [artists, setArtists] = useState([]);
-    
+
     return(
         <div></div>
     )
