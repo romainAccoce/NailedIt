74d31a341811b160fc1992a46735ab456cd1bed3
romainAccoce <romainaccoce@gmail.com>
2022-07-08 17:25:29 +0200

    add searchField component

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,11 +1,13 @@
 import './App.css';
 import Login from '../Login';
+import SearchField from '../Playlist';
 
 const App: React.FC = () => {
 
   return (
     <div className="App">
       <Login />
+      <SearchField />
     </div>
   );
 }
---
+++ src/components/SearchField/index.tsx
@@ -0,0 +1,12 @@
+import { useState } from "react";
+
+const SearchField: React.FC = () => {
+    const [searchKey, setSearchKey] = useState('');
+    const [artists, setArtists] = useState([]);
+    
+    return(
+        <div></div>
+    )
+};
+
+export default SearchField;
